61d8e90817272eb5bbc8e6a7ad98bea5978b41a9
coffee-r <game.developer.ryo@gmail.com>
2020-02-06 01:30:20 +0900

    [change]サイトタイトル表示

--- pages/index.vue
+++ pages/index.vue
@@ -11,21 +11,6 @@
       md6
     >
     <v-img src="title.png" aspect-ratio="2" contain></v-img>
-      <v-card>
-        <v-card-title class="headline">
-          工事中のこーひーあーるのポートフォリオサイトへよーこそ。
-        </v-card-title>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/Contact"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
     </v-flex>
   </v-layout>
 </template>
