0b3d7c8e0131901422e73efc462ee1f7d5ebc36d
coffee-r <game.developer.ryo@gmail.com>
2020-03-08 19:59:09 +0900

    [add]yozora-maker

--- pages/works.vue
+++ pages/works.vue
@@ -10,7 +10,8 @@
             <v-card-title>{{ work.title }}</v-card-title>
             <v-card-subtitle>{{ work.subtitle }}</v-card-subtitle>
             <v-card-actions>
-              <v-btn color="blue" text :disabled="work.linkdisable" :href="work.linkurl" target="_blank">{{ work.linkname}}</v-btn>
+              <v-btn color="blue" text :disabled="work.linkdisable1" :href="work.linkurl1" target="_blank">{{ work.linkname1}}</v-btn>
+              <v-btn color="purple" text :disabled="work.linkdisable2" :href="work.linkurl2" target="_blank">{{ work.linkname2}}</v-btn>
               <v-spacer></v-spacer>
               <v-btn icon @click="work.show = !work.show">
                 <v-icon>{{ work.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
@@ -45,9 +46,25 @@ export default {
           thumbnail: '../works/re_painter.png',
           explain: '親しみやすい見下ろし視点のスクロールアクションに描いたイラストが必殺技になるシステムを加えた短編2Dアクションゲーム。Unityを使い個人で制作をし、Steamにてリリース。',
           show: false,
-          linkdisable: false,
-          linkname: 'Store',
-          linkurl: 'https://store.steampowered.com/app/1096420/Re_Painter/',
+          linkdisable1: false,
+          linkname1: 'Store',
+          linkurl1: 'https://store.steampowered.com/app/1096420/Re_Painter/',
+          linkdisable2: true,
+          linkname2: '',
+          linkurl2: '',
+        },
+        {
+          title: 'YOZORA MAKER',
+          subtitle: 'ゲーム(PC-WebGL)',
+          thumbnail: '../works/yozora_maker.jpg',
+          explain: 'プレイヤー自身が撃った弾を避け続ける、物理演算を使った避けゲーム。',
+          show: false,
+          linkdisable1: false,
+          linkname1: 'Play',
+          linkurl1: 'https://unityroom.com/games/coffee-r-202002',
+          linkdisable2: false,
+          linkname2: 'GIT',
+          linkurl2: 'https://github.com/coffee-r/yozora-maker',
         },
         {
           title: 'ぱりんぱりん惑星',
@@ -55,9 +72,12 @@ export default {
           thumbnail: '../works/parinparin.png',
           explain: '四方から迫り来る星々を設置型トラップで爆破し、自陣の星をできる限り守り抜いてゆくカジュアルなタワーディフェンスゲーム。',
           show: false,
-          linkdisable: false,
-          linkname: 'Play',
-          linkurl: 'https://unityroom.com/games/parinparin',
+          linkdisable1: false,
+          linkname1: 'Play',
+          linkurl1: 'https://unityroom.com/games/parinparin',
+          linkdisable2: true,
+          linkname2: '',
+          linkurl2: '',
         },
         {
           title: 'Bingo Time Simulator',
@@ -65,9 +85,12 @@ export default {
           thumbnail: '../works/bingo.png',
           explain: '宴会でのビンゴの所要時間を予測するツール。JavaScriptを使ってブラウザ上で計算しています。自分が欲しかったので作りました。',
           show: false,
-          linkdisable: false,
-          linkname: 'Explore',
-          linkurl: 'https://coffee-ryo.devel.jp/app_Bingo.html',
+          linkdisable1: false,
+          linkname1: 'Explore',
+          linkurl1: 'https://coffee-ryo.devel.jp/app_Bingo.html',
+          linkdisable2: true,
+          linkname2: '',
+          linkurl2: '',
         },
         {
           title: 'Burning Snowman',
@@ -75,9 +98,12 @@ export default {
           thumbnail: '../works/snowman.png',
           explain: '人間を苦しめている寒さを止めるために、雪だるまが松明を持って永遠の冬を溶かす2Dのアクションアドベンチャー。',
           show: false,
-          linkdisable: false,
-          linkname: 'Play',
-          linkurl: 'https://unityroom.com/games/burning_snowman',
+          linkdisable1: false,
+          linkname1: 'Play',
+          linkurl1: 'https://unityroom.com/games/burning_snowman',
+          linkdisable2: true,
+          linkname2: '',
+          linkurl2: '',
         },     
         {
           title: '居合切り',
@@ -85,9 +111,12 @@ export default {
           thumbnail: '../works/iai.png',
           explain: '一対一でどちらが先に攻撃を当てられるかの駆け引きを行う対戦アクションゲーム。',
           show: false,
-          linkdisable: false,
-          linkname: 'Play',
-          linkurl: 'https://unityroom.com/games/coffee_iaigiri',
+          linkdisable1: false,
+          linkname1: 'Play',
+          linkurl1: 'https://unityroom.com/games/coffee_iaigiri',
+          linkdisable2: true,
+          linkname2: '',
+          linkurl2: '',
         },
         {
           title: 'オンラインテニス',
@@ -95,9 +124,12 @@ export default {
           thumbnail: '../works/online_tennis.png',
           explain: 'Unity×Photonを使った、PONGの通信対戦ができるゲーム。',
           show: false,
-          linkdisable: true,
-          linkname: '',
-          linkurl: '',
+          linkdisable1: true,
+          linkname1: '',
+          linkurl1: '',
+          linkdisable2: true,
+          linkname2: '',
+          linkurl2: '',
         }
       ]
     }
