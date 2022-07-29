e078462d83e01437f879ede0e3162475586db270
coffee-r <game.developer.ryo@gmail.com>
2022-07-29 23:52:27 +0900

    [add] ビンゴ計算シュミレーター

--- pages/bingo_time_simulator.vue
+++ pages/bingo_time_simulator.vue
@@ -20,8 +20,7 @@
 					</select>
 				</div>
 				<div class="mb-4 md:mb-6">
-					<label for="winningItemCount" class="block font-medium text-gray-900 dark:text-gray-300">景品数</label>
-					<label for="winningItemCount" class="block mb-2 font-medium text-gray-900 dark:text-gray-300">景品数は参加人数以下にしてください</label>
+					<label for="winningItemCount" class="block mb-2 font-medium text-gray-900 dark:text-gray-300">景品数</label>
 					<select id="winningItemCount" v-model="winningItemCount" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
 						<option v-for="(value, key) in 500" :key="key">{{ value }}</option>
 					</select>
@@ -88,30 +87,13 @@ export default {
 			isDisplaySimulationMessage: false
 		}
   },
-  computed: {
-    personCountAndWinningItemCount() {
-      return [this.$data.personCount, this.$data.winningItemCount];
-    }
-  },
   watch: {
-	personCountAndWinningItemCount(value, oldValue) {
-		this.personCount = this.personCountLimit(value[0]);
-		this.winningItemCount = this.winningItemCountLimit(value[1]);
-
-		// 賞品数を変えた時
-		if(value[1] !== oldValue[1]){
-			if(this.personCount < this.winningItemCount){
-				this.personCount = this.winningItemCount;
-			}
-		}
-
-		// 参加人数を変えた時
-		if(value[0] !== oldValue[0]){
-			if(this.winningItemCount > this.personCount){
-				this.winningItemCount = this.personCount;
-			}
-		}
-    },
+	personCount(input) {
+		this.personCount = this.personCountLimit(input);
+	},
+	winningItemCount(input) {
+		this.winningItemCount = this.pwinningItemCountLimit(input);
+	},
 	lotteryTime(input) {
 		this.lotteryTime = this.timeLimit(input);
 	},
@@ -124,18 +106,22 @@ export default {
 	personCountLimit(input){
 		if(input < 1) {return 1;}
 		if(input > 500) {return 500;}
-		return input;
+		return parseInt(input);
 	},
-	winningItemCountLimit(input){
+	pwinningItemCountLimit(input){
 		if(input < 1) {return 1;}
 		if(input > 500) {return 500;}
-		return input;
+		return parseInt(input);
 	},
 	timeLimit(input){
 		if(input < 1) {return 1;}
-		return input;
+		return parseInt(input);
 	},
 	simulate(){
+		if(this.personCount < this.winningItemCount){
+			alert("景品数を参加人数以下にしてください");
+			return;
+		}
 		let bingoTournamentCollection = new BingoTournamentCollection([]);
 		for(let i=0; i<100; i++){
 			bingoTournamentCollection = bingoTournamentCollection.add(new BingoTournament(BingoSheetFactory, new LotteryBingoNumberStack(), this.personCount, this.winningItemCount, this.lotteryTime, this.winningTime));
