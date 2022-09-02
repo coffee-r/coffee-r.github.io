e38a931cf78a2a95e00e613eda61626d506d2cb8
coffee-r <coffee-r@users.noreply.github.com>
2022-09-02 15:44:51 +0000

    deploy: 4acfd5fb3776fccaede371c544e65e48e1b52198

--- _nuxt/f796e73.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{282:function(t,e,n){"use strict";n.r(e);var c=n(46),component=Object(c.a)({},(function(){this._self._c;return this._m(0)}),[function(){var t=this._self._c;return t("footer",{staticClass:"mt-4 mb-2 text-center"},[t("p",[this._v("© 2022 coffee-r")])])}],!1,null,null,null);e.default=component.exports}}]);
